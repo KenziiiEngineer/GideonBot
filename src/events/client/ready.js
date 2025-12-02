4d8315f0f2585458842fa76cf866b969f03d65d3
KenziiiEngineer <kenziiicontacto@gmail.com>
2025-12-02 16:04:00 -0600

    Delete src directory

--- src/events/client/ready.js
+++
@@ -1,8 +0,0 @@
-export default {
-  name: "ready",
-  once: true,
-  execute(client) {
-    console.log(`✅ Sesión iniciada como ${client.user.tag}`);
-    client.user.setActivity("Gideon Studio", { type: 3 });
-  }
-};
--- src/utils/logger.js
+++
@@ -1,4 +0,0 @@
-export function logWithTime(message) {
-  const date = new Date().toLocaleString("es-MX", { hour12: false });
-  console.log(`[${date}] ${message}`);
-}
